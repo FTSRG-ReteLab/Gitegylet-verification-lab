72d988c86139ce8fbad55a35eeb2bd7c30d8b0e9
Akos Szabados <szabados.akos@gmail.com>
2017-03-20 08:48:16 -0700

    implemented alarm feature and unit test

--- train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
+++ train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
@@ -3,17 +3,53 @@ package hu.bme.mit.train.sensor;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+
+import hu.bme.mit.train.interfaces.TrainController;
+import hu.bme.mit.train.interfaces.TrainUser;
+
 import static org.mockito.Mockito.*;
 
 public class TrainSensorTest {
-
+	TrainSensorImpl sensor;
+	TrainUser mockTU;
+	TrainController mockTC;
+	
     @Before
     public void before() {
-        // TODO Add initializations
+    	mockTU = mock(TrainUser.class);
+    	mockTC = mock(TrainController.class);
+    	sensor = new TrainSensorImpl(mockTC, mockTU);
     }
 
     @Test
-    public void ThisIsAnExampleTestStub() {
-        // TODO Delete this and add test cases based on the issues
+    public void CheckAlarmState_IfSpeedLimitIsBelowZero() {
+    	sensor.overrideSpeedLimit(-10);
+    	verify(mockTU, times(1)).setAlarmState(true);
+    }
+    
+    @Test
+    public void CheckAlarmState_IfSpeedLimitIsOver500() {
+    	sensor.overrideSpeedLimit(510);
+    	verify(mockTU, times(1)).setAlarmState(true);
+    }
+    
+    @Test
+    public void CheckAlarmState_IfSpeedLimitIsJustOver50PercentOfReferenceSpeed() {
+        when(mockTC.getReferenceSpeed()).thenReturn(100);
+        
+        sensor.overrideSpeedLimit(51);
+        
+        verify(mockTC, times(1)).getReferenceSpeed();
+        verify(mockTU, times(1)).setAlarmState(false);
+    }
+    
+    @Test
+    public void CheckAlarmState_IfSpeedLimitIsJustBelow50PercentOfReferenceSpeed() {
+        when(mockTC.getReferenceSpeed()).thenReturn(100);
+        
+        sensor.overrideSpeedLimit(49);
+        
+        verify(mockTC, times(1)).getReferenceSpeed();
+        verify(mockTU, times(1)).setAlarmState(true);
     }
 }
